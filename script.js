16f4a9589a72a300e17dde6f79c88b95509d9ee0
zenonass <zenonasauzbikas@gmail.com>
2021-07-20 21:55:24 +0300

    Added functionality to change the grid dimensions with slider

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const grid = document.querySelector(".grid-container")
+const grid = document.querySelector(".grid-container");
+const steplist = document.querySelector("#steplist-slider");
+const resetButton = document.querySelector("#reset-button");
 
-
-let gridDimensions = prompt('Choose the dimensions of a grid');
-
-for (let item = 0; item < Math.pow(gridDimensions,2); item++) {
-    const newGridItem = document.createElement('div')
-
-    newGridItem.classList.add("grid-item")
-
-    grid.appendChild(newGridItem)
-    
+function makeGrid(gridDimensions){
+    grid.innerHTML='';
+    for (let item = 0; item < Math.pow(gridDimensions,2); item++) {
+        const newGridItem = document.createElement('div');
+        newGridItem.classList.add("grid-item")
+        grid.appendChild(newGridItem)
+    }
+    grid.style["grid-template-columns"] = `repeat(${gridDimensions}, auto)`
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(function(gridItem){
+            gridItem.addEventListener('mouseover', function(e){
+                e.target.style.background = 'yellow';
+            });
+});
 }
 
-grid.style["grid-template-columns"] = `repeat(${gridDimensions}, auto)`;
-
-const gridItems = document.querySelectorAll(".grid-item")
-
-gridItems.forEach(function(gridItem){
-    gridItem.addEventListener('mouseover', function(e){
-        e.target.style.background = 'blue';
-    });
+steplist.addEventListener('change', function(e){
+    let gridDimensions= e.target.value;
+    makeGrid(gridDimensions)
 });
 
-
-
-const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener('click', ()=>{
+    const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach(gridItem => {
         gridItem.style.background = 'white';
     });
-})
+});
 
+makeGrid(10)
